e9611afb84924b35e52909c1f2fd1dd14bda2052
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-12-19 22:11:12 +0100

    Milestone 3

--- form.php
+++ form.php
@@ -12,6 +12,15 @@
     <form action="password.php">
         <label for="length">Inserisci la lunghezza della password</label>
         <input id="length" name="length" type="number">
+        <select name="whichCharacters">
+            <option value="0">Lettere</option>
+            <option value="1">Numeri</option>
+            <option value="2">Caratteri</option>
+            <option value="0,1">Lettere e numeri</option>
+            <option value="0,2">Lettere e caratteri</option>
+            <option value="1,2">Numeri e caratteri</option>
+            <option value="0,1,2">Lettere, numeri e caratteri</option>
+        </select>
         <button type="submit">Genera</button>
     </form>
 </body>
--- functions.php
+++ functions.php
@@ -1,11 +1,22 @@
 <?php
 function generatePassword()
 {
-    $characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "`", "!", '"', "?", "$", "?", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ":", ";", "@", "'", "~", "#", "|", "\\", "<", ">", ".", "?", "/"];
+    $characters = [
+        ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+        ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+        ["`", "!", '"', "?", "$", "?", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ":", ";", "@", "'", "~", "#", "|", "\\", "<", ">", ".", "?", "/"]
+    ];
+    $which_characters_explode = explode(",", $_GET["whichCharacters"]);
+    $characters_allowed = [];
+    for ($i = 0; $i < sizeof($which_characters_explode); $i++) {
+        for ($index = 0; $index < sizeof($characters[$which_characters_explode[$i]]); $index++) {
+            $characters_allowed[] = $characters[$which_characters_explode[$i]][$index];
+        }
+    }
     $password = "";
     for ($i = 0; $i < $_GET["length"]; $i++) {
-        $randomNumber = rand(0, (sizeof($characters) - 1));
-        $password = $password . $characters[$randomNumber];
+        $randomNumber = rand(0, (sizeof($characters_allowed) - 1));
+        $password = $password . $characters_allowed[$randomNumber];
     }
     echo "La tua nuova password è <strong>" . $password . "</strong>";
 }
