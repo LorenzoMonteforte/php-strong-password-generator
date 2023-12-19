b553593a8ebc66fe18ca02f95cc00e1770acc15e
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-12-19 21:06:12 +0100

    Milestone 1

---
+++ form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="form.css">
+</head>
+
+<body>
+    <form action="password.php">
+        <label for="length">Inserisci la lunghezza della password</label>
+        <input id="length" name="length" type="number">
+        <button type="submit">Genera</button>
+    </form>
+</body>
+
+</html>
---
+++ password.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="password.css">
+    <?php
+    function generatePassword()
+    {
+        $characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "`", "!", '"', "?", "$", "?", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ":", ";", "@", "'", "~", "#", "|", "\\", "<", ">", ".", "?", "/"];
+        $password = "";
+        for ($i = 0; $i < $_GET["length"]; $i++) {
+            $randomNumber = rand(0, (sizeof($characters) - 1));
+            $password = $password . $characters[$randomNumber];
+        }
+        echo "La tua nuova password è <strong>" . $password . "</strong>";
+    }
+    ?>
+</head>
+
+<body>
+    <p>
+        <?php
+        generatePassword();
+        ?>
+    </p>
+</body>
+
+</html>
